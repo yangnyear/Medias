e58d0ba572112198eecd6255155148524cde6316
羊荣毅_L <yangrongyi_em@163.com>
2017-03-21 18:28:38 +0800

    自定义view

--- app/src/main/java/com/swpuiot/medias/view/HorizontalScrollViewEX.java
+++ app/src/main/java/com/swpuiot/medias/view/HorizontalScrollViewEX.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Scroller;
 
@@ -12,7 +13,7 @@ import android.widget.Scroller;
  */
 public class HorizontalScrollViewEX extends ViewGroup {
     private int mChildrenSize;
-    private int mChildrenWiden;
+    private int mChildrenWidth;
     private int mChildendex;
 
     //分别记录上次滑动的坐标
@@ -99,9 +100,9 @@ public class HorizontalScrollViewEX extends ViewGroup {
                 }
                 break;
             }
-            case MotionEvent.ACTION_MOVE: {
-                int deltX = x - mLastX;
-                int deltY = y - mLastY;
+            case MotionEvent.ACTION_MOVE: {//当按住控件拖动时
+                int deltX = x - mLastX;//x方向拖动距离
+                int deltY = y - mLastY;//y方向拖动距离
                 scrollBy(-deltX, 0);
                 break;
             }
@@ -112,10 +113,10 @@ public class HorizontalScrollViewEX extends ViewGroup {
                 if (Math.abs(xVeloCity) >= 50) {
                     mChildendex = xVeloCity > 0 ? mChildendex - 1 : mChildendex + 1;
                 } else {
-                    mChildendex = (scrollX + mChildrenWiden / 2) / mChildrenWiden;
+                    mChildendex = (scrollX + mChildrenWidth / 2) / mChildrenWidth;
                 }
                 mChildendex = Math.max(0, Math.min(mChildendex, mChildrenSize - 1));
-                int dX = mChildendex * mChildrenWiden - scrollX;
+                int dX = mChildendex * mChildrenWidth - scrollX;
                 smoothScrollBy(dX, 0);
                 mVelocityTracker.clear();
                 break;
@@ -134,20 +135,64 @@ public class HorizontalScrollViewEX extends ViewGroup {
         int measureedWidth = 0;
         int measureedHeight = 0;
         final int childrenCount = getChildCount();
-        measureChildren(widthMeasureSpec,heightMeasureSpec);
-        
-        int widthSpaceSize=MeasureSpec.getSize(widthMeasureSpec);
-        // TODO: 2017/3/20  
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
 
+        int widthSpaceSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthSpaceMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightSpaceSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heightSpaceMode = MeasureSpec.getMode(heightMeasureSpec);
+        if (childrenCount == 0) {
+            setMeasuredDimension(0, 0);
+        } else if (widthSpaceMode == MeasureSpec.AT_MOST && heightSpaceMode == MeasureSpec.AT_MOST) {
+            final View childView = getChildAt(0);
+            measureedWidth = childView.getMeasuredWidth() * childrenCount;
+            measureedHeight = childView.getMeasuredHeight();
+            setMeasuredDimension(measureedWidth, measureedHeight);
+        } else if (widthSpaceMode == MeasureSpec.AT_MOST) {
+            final View childView = getChildAt(0);
+            measureedWidth = childView.getMeasuredWidth() * childrenCount;
+            setMeasuredDimension(measureedWidth, heightSpaceSize);
+        } else if (heightSpaceMode == MeasureSpec.AT_MOST) {
+            final View childView = getChildAt(0);
+            measureedHeight = childView.getMeasuredHeight();
+            setMeasuredDimension(widthSpaceSize,measureedHeight);
+        }
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int childLeft = 0;
+        final int childCount = getChildCount();
+        mChildrenSize = childCount;
+        for (int i = 0; i < childCount; i++) {
+            final View childView = getChildAt(i);
+            if (childView.getVisibility() != View.GONE) {
+                final int childWidth = childView.getMeasuredWidth();
+                mChildrenWidth = childWidth;
 
+                //放在布局里面的控件的位置(左,上,右,下)
+                childView.layout(childLeft, 30, (childLeft + childWidth), childView.getMeasuredHeight());
+                childLeft += childWidth;
+            }
+        }
     }
 
     private void smoothScrollBy(int dX, int dY) {
-        mScanner.startScroll(getScrollX(), 0, dX, 0, 500);
+        mScanner.startScroll(getScrollX(), 0, dX, dY, 500);
         invalidate();
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mScanner.computeScrollOffset()) {
+            scrollTo(mScanner.getCurrX(), mScanner.getCurrY());
+            postInvalidate();
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        mVelocityTracker.recycle();
+        super.onDetachedFromWindow();
     }
 }
